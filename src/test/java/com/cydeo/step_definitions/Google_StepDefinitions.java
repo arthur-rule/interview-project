48f1b317c27d536e990f1934acd55d47ea80da15
arthur-rule <arthur.rule@cydeo.com>
2024-05-09 13:59:24 +0300

    Initial commit

---
+++ src/test/java/com/cydeo/step_definitions/Google_StepDefinitions.java
@@ -0,0 +1,16 @@
+package com.cydeo.step_definitions;
+
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Google_StepDefinitions {
+
+    @When("user is on the Google search page")
+    public void user_is_on_the_google_search_page() {
+        Driver.getDriver().get("https://www.google.com");
+    }
+    @Then("user should see title is Google")
+    public void user_should_see_title_is_google() {
+    }
+}
